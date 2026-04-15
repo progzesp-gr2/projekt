8235d0b6b645d4e432f44b5e040a564fda536c8e
Witold Węglarz <wrglarz25@gmail.com>
2026-04-15 19:28:02 +0200

    initial test cases

---
+++ backend/scrum/tests/test_auth.py
@@ -0,0 +1,77 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model, get_user
+from django.urls import reverse
+
+User = get_user_model()
+
+# Create your tests here.
+
+class ApiRegisterTestCase(TestCase):
+	def test1(self):
+		data = {'username': 'jtp', 'password': 'test', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
+
+		response = self.client.post(
+			path=reverse('auth-register'), 
+			data=data
+		)
+
+		self.assertEqual(response.status_code, 201)
+
+		del data['password']
+
+		user = User.objects.get(pk=1)
+		rd = response.json()
+		for key, value in data.items():
+			self.assertEqual(rd[key], value)
+			self.assertEqual(user.__dict__[key], value)
+
+
+class ApiAuthTestCase(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		cls.userdata = {'username': 'jtp', 'password': 'pass', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
+		User.objects.create_user(**cls.userdata)
+		del cls.userdata['password']
+
+	def test_login(self):
+		response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp', 'password': 'pass'})
+
+		self.assertEqual(response.status_code, 200)
+
+		user = get_user(self.client)
+
+		self.assertTrue(user.is_authenticated)
+		for key, value in self.userdata.items():
+			self.assertEqual(user.__dict__[key], value)
+		
+	def test_me(self):
+		self.assertTrue(self.client.login(username='jtp', password='pass'))
+
+		response = self.client.get(reverse('auth-me'))
+
+		self.assertEqual(response.status_code, 200)
+
+		rd = response.json()
+		for key, value in self.userdata.items():
+			self.assertEqual(rd[key], value)
+
+	def test_logout(self):
+		self.assertTrue(self.client.login(username='jtp', password='pass'))
+
+		response = self.client.post(reverse('auth-logout'))
+
+		self.assertEqual(response.status_code, 204)
+
+		user = get_user(self.client)
+
+		self.assertFalse(user.is_authenticated)
+	
+	def test_nologin(self):
+		response = self.client.get(path=reverse('auth-me'))
+		self.assertEqual(response.status_code, 403)
+
+	def test_login_missing(self):
+		response = self.client.post(path=reverse('auth-login'))
+		self.assertEqual(response.status_code, 400)
+		response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp'})
+		self.assertEqual(response.status_code, 400)
