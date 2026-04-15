019199c617e7a11185f2e71e297b118e3284dcdb
Witold Węglarz <wrglarz25@gmail.com>
2026-04-15 20:21:09 +0200

    formatowanie

--- backend/scrum/tests/test_auth.py
+++ backend/scrum/tests/test_auth.py
@@ -7,71 +7,71 @@ User = get_user_model()
 # Create your tests here.
 
 class ApiRegisterTestCase(TestCase):
-	def test1(self):
-		data = {'username': 'jtp', 'password': 'test', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
+    def test1(self):
+        data = {'username': 'jtp', 'password': 'test', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
 
-		response = self.client.post(
-			path=reverse('auth-register'), 
-			data=data
-		)
+        response = self.client.post(
+            path=reverse('auth-register'), 
+            data=data
+        )
 
-		self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 201)
 
-		del data['password']
+        del data['password']
 
-		user = User.objects.get(pk=1)
-		rd = response.json()
-		for key, value in data.items():
-			self.assertEqual(rd[key], value)
-			self.assertEqual(user.__dict__[key], value)
+        user = User.objects.get(pk=1)
+        rd = response.json()
+        for key, value in data.items():
+            self.assertEqual(rd[key], value)
+            self.assertEqual(user.__dict__[key], value)
 
 
 class ApiAuthTestCase(TestCase):
-	@classmethod
-	def setUpTestData(cls):
-		cls.userdata = {'username': 'jtp', 'password': 'pass', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
-		User.objects.create_user(**cls.userdata)
-		del cls.userdata['password']
+    @classmethod
+    def setUpTestData(cls):
+        cls.userdata = {'username': 'jtp', 'password': 'pass', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
+        User.objects.create_user(**cls.userdata)
+        del cls.userdata['password']
 
-	def test_login(self):
-		response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp', 'password': 'pass'})
+    def test_login(self):
+        response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp', 'password': 'pass'})
 
-		self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-		user = get_user(self.client)
+        user = get_user(self.client)
 
-		self.assertTrue(user.is_authenticated)
-		for key, value in self.userdata.items():
-			self.assertEqual(user.__dict__[key], value)
-		
-	def test_me(self):
-		self.assertTrue(self.client.login(username='jtp', password='pass'))
+        self.assertTrue(user.is_authenticated)
+        for key, value in self.userdata.items():
+            self.assertEqual(user.__dict__[key], value)
+        
+    def test_me(self):
+        self.assertTrue(self.client.login(username='jtp', password='pass'))
 
-		response = self.client.get(reverse('auth-me'))
+        response = self.client.get(reverse('auth-me'))
 
-		self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-		rd = response.json()
-		for key, value in self.userdata.items():
-			self.assertEqual(rd[key], value)
+        rd = response.json()
+        for key, value in self.userdata.items():
+            self.assertEqual(rd[key], value)
 
-	def test_logout(self):
-		self.assertTrue(self.client.login(username='jtp', password='pass'))
+    def test_logout(self):
+        self.assertTrue(self.client.login(username='jtp', password='pass'))
 
-		response = self.client.post(reverse('auth-logout'))
+        response = self.client.post(reverse('auth-logout'))
 
-		self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, 204)
 
-		user = get_user(self.client)
+        user = get_user(self.client)
 
-		self.assertFalse(user.is_authenticated)
-	
-	def test_nologin(self):
-		response = self.client.get(path=reverse('auth-me'))
-		self.assertEqual(response.status_code, 403)
+        self.assertFalse(user.is_authenticated)
+    
+    def test_nologin(self):
+        response = self.client.get(path=reverse('auth-me'))
+        self.assertEqual(response.status_code, 403)
 
-	def test_login_missing(self):
-		response = self.client.post(path=reverse('auth-login'))
-		self.assertEqual(response.status_code, 400)
-		response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp'})
-		self.assertEqual(response.status_code, 400)
+    def test_login_missing(self):
+        response = self.client.post(path=reverse('auth-login'))
+        self.assertEqual(response.status_code, 400)
+        response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp'})
+        self.assertEqual(response.status_code, 400)
--- backend/scrum/tests/test_sprints.py
+++ backend/scrum/tests/test_sprints.py
@@ -6,65 +6,65 @@ from ..models import Project, Sprint
 User = get_user_model()
 
 class ApiSprintNoLoginTestCase(TestCase):
-	def test_nologin(self):
-		response = self.client.get(path=reverse('task-list-create'))
-		self.assertEqual(response.status_code, 403)
+    def test_nologin(self):
+        response = self.client.get(path=reverse('task-list-create'))
+        self.assertEqual(response.status_code, 403)
 
 class ApiSprintCreateTestCase(TestCase):
-	@classmethod
-	def setUpTestData(cls) -> None:
-		userdata = {'username': 'jtp', 'password': 'pass', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
-		cls.user = User.objects.create_user(**userdata)
+    @classmethod
+    def setUpTestData(cls) -> None:
+        userdata = {'username': 'jtp', 'password': 'pass', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
+        cls.user = User.objects.create_user(**userdata)
 
-		cls.project = Project.objects.create(name='Foo', key='foo', description='Lorem ipsum dolor sit amet', owner=cls.user)
+        cls.project = Project.objects.create(name='Foo', key='foo', description='Lorem ipsum dolor sit amet', owner=cls.user)
 
-	def setUp(self) -> None:
-		self.client.login(username='jtp', password='pass')
+    def setUp(self) -> None:
+        self.client.login(username='jtp', password='pass')
 
-	def test_create(self):
-		data = {
-			'name': 'Test Sprint',
-			'project': 1
-		}
+    def test_create(self):
+        data = {
+            'name': 'Test Sprint',
+            'project': 1
+        }
 
-		response = self.client.post(path=reverse('sprint-list-create'), data=data)
+        response = self.client.post(path=reverse('sprint-list-create'), data=data)
 
-		self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 201)
 
-		sprint = Sprint.objects.get(pk=1)
+        sprint = Sprint.objects.get(pk=1)
 
-		self.assertEqual(sprint.project, self.project)
-		self.assertEqual(sprint.name, data['name'])
-		self.assertIn(sprint, self.project.sprints.all())
+        self.assertEqual(sprint.project, self.project)
+        self.assertEqual(sprint.name, data['name'])
+        self.assertIn(sprint, self.project.sprints.all())
 
-		rd = response.json()
+        rd = response.json()
 
-		self.assertEqual(rd['name'], data['name'])
-	
-	def test_missing(self):
-		response = self.client.post(path=reverse('sprint-list-create'))
-		self.assertEqual(response.status_code, 400)
+        self.assertEqual(rd['name'], data['name'])
+    
+    def test_missing(self):
+        response = self.client.post(path=reverse('sprint-list-create'))
+        self.assertEqual(response.status_code, 400)
 
 class ApiSprintListTestCase(TestCase):
-	@classmethod
-	def setUpTestData(cls) -> None:
-		userdata = {'username': 'jtp', 'password': 'pass', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
-		cls.user = User.objects.create_user(**userdata)
+    @classmethod
+    def setUpTestData(cls) -> None:
+        userdata = {'username': 'jtp', 'password': 'pass', 'first_name': 'Jan', 'last_name': 'Tępy', 'email': 'jtp@example.com'}
+        cls.user = User.objects.create_user(**userdata)
 
-		cls.project = Project.objects.create(name='Foo', key='foo', description='Lorem ipsum dolor sit amet', owner=cls.user)
-		cls.sprints = [
-			Sprint.objects.create(name='Sprint 1', project=cls.project),
-			Sprint.objects.create(name='Sprint 2', project=cls.project)
-		]
+        cls.project = Project.objects.create(name='Foo', key='foo', description='Lorem ipsum dolor sit amet', owner=cls.user)
+        cls.sprints = [
+            Sprint.objects.create(name='Sprint 1', project=cls.project),
+            Sprint.objects.create(name='Sprint 2', project=cls.project)
+        ]
 
-	def setUp(self) -> None:
-		self.client.login(username='jtp', password='pass')
+    def setUp(self) -> None:
+        self.client.login(username='jtp', password='pass')
 
-	def test_get_list(self):
-		response = self.client.get(path=reverse('sprint-list-create'))
+    def test_get_list(self):
+        response = self.client.get(path=reverse('sprint-list-create'))
 
-		self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-		rd = response.json()
+        rd = response.json()
 
-		self.assertTrue((rd[0]['name'] == 'Sprint 1' and rd[1]['name'] == 'Sprint 2') or (rd[1]['name'] == 'Sprint 1' and rd[0]['name'] == 'Sprint 2'))
+        self.assertTrue((rd[0]['name'] == 'Sprint 1' and rd[1]['name'] == 'Sprint 2') or (rd[1]['name'] == 'Sprint 1' and rd[0]['name'] == 'Sprint 2'))
