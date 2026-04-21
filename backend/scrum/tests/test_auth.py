1a972a5a2ed79e07f8a75ad7813ed3fc4edbb9f3
Witold Węglarz <wrglarz25@gmail.com>
2026-04-21 20:21:37 +0200

    testy złych danych do auth

--- backend/scrum/tests/test_auth.py
+++ backend/scrum/tests/test_auth.py
@@ -75,3 +75,13 @@ class ApiAuthTestCase(TestCase):
         self.assertEqual(response.status_code, 400)
         response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp'})
         self.assertEqual(response.status_code, 400)
+    
+    def test_bad_user(self):
+        response = self.client.post(path=reverse('auth-login'), data={'username': 'nope', 'password': 'pass'})
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_bad_password(self):
+        response = self.client.post(path=reverse('auth-login'), data={'username': 'jtp', 'password': 'nope'})
+
+        self.assertIn(response.status_code, (400, 401))
