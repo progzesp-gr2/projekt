90d829621d26f2695a07ab1d1b992f2b962c1148
Witold Węglarz <wrglarz25@gmail.com>
2026-04-22 14:45:32 +0200

    pierwszy test do listy zadań

--- backend/scrum/tests/test_tasks.py
+++ backend/scrum/tests/test_tasks.py
@@ -2,6 +2,7 @@ from ..models import Project, Sprint, Task
 from django.contrib.auth import get_user_model, get_user
 from django.test import TestCase
 from django.urls import reverse
+from datetime import date
 
 User = get_user_model()
 
@@ -107,3 +108,69 @@ class ApiTaskCreateTestCase(TestCase):
 
         self.assertEqual(rd['reporter'], 1)
         self.assertEqual(task.reporter, self.user)
+
+class ApiTaskListTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='jtp', password='pass', first_name='Jan', last_name='Tępy', email='jtp@example.com')
+        cls.project = Project.objects.create(name='Foo', key='foo', description='Lorem ipsum dolor sit amet', owner=cls.user)
+        cls.sprint = Sprint.objects.create(
+            name='Sprint 1',
+            project=cls.project,
+            goal='Goal 1',
+            start_date=date(2026, 1, 1),
+            end_date=date(2026, 1, 15),
+            status=Sprint.SprintStatus.ACTIVE
+        )
+
+        cls.tasks = [
+            Task.objects.create(
+                title='Task 1',
+                project=cls.project,
+                sprint=cls.sprint,
+                reporter=cls.user,
+                assignee=cls.user,
+                description='desc 1',
+                task_type=Task.TaskType.STORY,
+                status=Task.TaskStatus.TODO,
+                priority=Task.TaskPriority.LOW
+            ),
+            Task.objects.create(
+                title='Task 2',
+                project=cls.project,
+                sprint=cls.sprint,
+                reporter=cls.user,
+                assignee=cls.user,
+                description='desc 2',
+                task_type=Task.TaskType.BUG,
+                status=Task.TaskStatus.IN_PROGRESS,
+                priority=Task.TaskPriority.MEDIUM
+            )
+        ]
+    
+    def setUp(self) -> None:
+        self.assertTrue(self.client.login(username='jtp', password='pass'))
+
+    def test_get_list(self):
+        response = self.client.get(path=reverse('task-list-create'))
+
+        self.assertEqual(response.status_code, 200)
+
+        rd = response.json()
+
+        for task in rd:
+            self.assertEqual(task['project'], self.project.pk)
+            self.assertEqual(task['reporter'], self.user.pk)
+            match task['title']:
+                case 'Task 1':
+                    self.assertEqual(task['description'], 'desc 1')
+                    self.assertEqual(task['task_type'], 'STORY')
+                    self.assertEqual(task['status'], 'TODO')
+                    self.assertEqual(task['priority'], 'LOW')
+                case 'Task 2':
+                    self.assertEqual(task['description'], 'desc 2')
+                    self.assertEqual(task['task_type'], 'BUG')
+                    self.assertEqual(task['status'], 'IN_PROGRESS')
+                    self.assertEqual(task['priority'], 'MEDIUM')
+                case _:
+                    self.fail()
