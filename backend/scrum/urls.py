d8ec2ac8311542a066a0928a7c49e4849e1f41e3
Dinolek <git@dinolek.me>
2026-05-06 19:33:37 +0200

    backend: crudify project endpoints
    Signed-off-by: Dinolek <git@dinolek.me>

--- backend/scrum/urls.py
+++ backend/scrum/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('api/auth/me', views.MeView.as_view(), name='auth-me'),
 
     path('api/projects/', views.ProjectListCreateView.as_view(), name='project-list-create'),
+    path('api/projects/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
     path('api/tasks/', views.TaskListCreateView.as_view(), name='task-list-create'),
-    path('api/tasks/<int:pk>', views.TaskUpdateView.as_view(), name='task-update'),
+    path('api/tasks/<int:pk>', views.TaskDetailView.as_view(), name='task-detail'),
     path('api/sprints/', views.SprintListCreateView.as_view(), name='sprint-list-create'),
+    path('api/sprints/<int:pk>', views.SprintDetailView.as_view(), name='sprint-detail'),
 ]
