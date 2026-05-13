cbdd2aa862ff013e2b02a4668c01b52df5129669
dieselP273 <kuba.spamm@gmail.com>
2026-05-13 22:18:36 +0200

    Update frontend/src/components/BacklogTab.jsx
    Co-authored-by: Jan Markiewicz <j.markiewicz@student.uj.edu.pl>

--- frontend/src/components/BacklogTab.jsx
+++ frontend/src/components/BacklogTab.jsx
@@ -5,7 +5,7 @@
 
 import { useState } from 'react';
 
-export default function BacklogTab() {
+export default function BacklogTab({projectId}) {
 
   const [editingTaskId, setEditingTaskId] = useState(null);
 
